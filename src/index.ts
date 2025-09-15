ef8fea137dfe12c31905ca3739023a57f6c6c6b2
Grant <gccody2010@live.com>
2025-09-15 14:20:06 -0400

    Removed any unused remnants

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 /// <reference types="@cloudflare/workers-types" />
 
 import { FaviconFetcher } from './faviconFetcher';
-interface Env {
-  // No longer need KV or R2
-}
 
 const defaultSvg = `<svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
  <path d="M12 2C14.5013 4.73835 15.9228 8.29203 16 12C15.9228 15.708 14.5013 19.2616 12 22M12 2C9.49872 4.73835 8.07725 8.29203 8 12C8.07725 15.708 9.49872 19.2616 12 22M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22M2.50002 9H21.5M2.5 15H21.5" stroke="#FFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>`;
 
 export default {
-  async fetch(request: Request, env: Env): Promise<Response> {
+  async fetch(request: Request): Promise<Response> {
     const url = new URL(request.url);
 
     if (url.pathname === '/favicon') {
